4ca3604d56e16a6a1125115211805f31d585b84c
jshuckbot <gorka_ivan@mail.ru>
2023-04-26 14:19:44 +0700

    ready task 2

--- less_3/1.js
+++ less_3/1.js
@@ -1,8 +1,10 @@
+'use strict';
+
 function convertCelsiusToFahrenheit(temperature) {
-    return  Math.round((1.8 * temperature + 32) * 100) / 100
+    return  Math.round((1.8 * temperature + 32) * 100) / 100;
 }
 
-const temperatureCelsius = Number.parseFloat(prompt('Введите температуру в градусах Цельсия'))
-const temperatureFahrenheit = convertCelsiusToFahrenheit(temperatureCelsius)
+const temperatureCelsius = Number.parseFloat(prompt('Введите температуру в градусах Цельсия'));
+const temperatureFahrenheit = convertCelsiusToFahrenheit(temperatureCelsius);
 
-alert(`Цельсий: ${temperatureCelsius} Фаренгейт: ${temperatureFahrenheit}`)
+alert(`Цельсий: ${temperatureCelsius} Фаренгейт: ${temperatureFahrenheit}`);
